20a551390ed5285610e74be5f6417db486f4450b
hxg <842429313@qq.com>
2022-04-18 11:30:35 +0800

    4月18日leetcode刷题

---
+++ 386_2.go
@@ -0,0 +1,20 @@
+package main
+
+func lexicalOrder(n int) []int {
+	ans := make([]int, n)
+	num := 1
+	for i := range ans {
+		ans[i] = num
+		if num*10 < n {
+			num *= 10
+			continue
+		} else {
+			if num%10 == 9 || num+1 > n {
+				num /= 10
+			}
+			num += 1
+		}
+
+	}
+	return ans
+}
